e847d995019bfead8af9571a1b0fa23ae5608e07
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-28 00:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function firstWord(s) {
   // your code here
 
 	
+
 	
 	let str =s.split(" ");
-	let str2="'"
-	str2+= str[0];
-	str2+="'";
-	return str2;
+	// let str2="'\'"
+	// str2+= str[0];
+	// str2+="'";
+	return str[0];
 }
+
 
 // Do not change the code below
 
