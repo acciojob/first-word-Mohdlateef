93b8c8466b6f672d88c09b503f62cbc8013d111b
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-27 23:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function firstWord(s) {
   // your code here
+
+	
+	
 	let str =s.split(" ");
-	return str[0];
+	let str2="'"
+	str2+= str[0];
+	str2+="'";
+	return str2;
 }
 
 // Do not change the code below
+
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
