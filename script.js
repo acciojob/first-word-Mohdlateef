fd708553d405c89148e0101af45afd0a993ab98c
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-14 20:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 function firstWord(s) {
   // your code here
+	let flag=0;
+	if(s.length===0)
+	{
+		let str1="''"
+		
+		return str1;
+	}
+let str="'"
+	for(let i=0;i<s.length;i++)
+	{	
+		if(s[i]>='A'&&s[i]<='Z'||s[i]>='a'&&s[i]<='z')
+		{
+			str=str+s[i];
+			flag =1;
+		}
+		else if(flag===1)
+		{
+			break;
+		}
 
+}
+	str=str+"'"
+	return str;
 	
-	
-let arr=	s.split(" ");
-	let str2=`'${arr[0]}'`
-	
-	return str2;
 }
 
 
 // Do not change the code below
+
 
 
 const s = prompt("Enter String:");
